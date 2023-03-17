04a8eaf12173106ec47be6731c546f15f38c4b9e
Anas Sulaiman <anas@sulaiman.pro>
2023-03-17 18:42:57 +0000

    WIP: missing blobs

--- go/pkg/batcher/batcher.go
+++ go/pkg/batcher/batcher.go
@@ -5,44 +5,61 @@ import (
 	"fmt"
 )
 
+// SizeFunc is callback that can return the size of element at index i.
 type SizeFunc func(i int) int64
 
-var ErrItemTooLarge = errors.New("item too large, cannot fit into a batch")
-var ErrInvalidArgs = errors.New("invalid arguments, impossible to make a batch")
+var (
+	// ErrItemTooLarge indicates an item that cannot fit into a single batch.
+	ErrItemTooLarge = errors.New("item too large, cannot fit into a batch")
 
+	// ErrInvalidArgument indicates an invalid value for an argument.
+	ErrInvalidArgument = errors.New("invalid argument")
+)
+
+// Make returns a slice of batches, each contains the indices of the elements that should
+// be included in that batch.
+// It does so using a simple forward scan, opening a new batch each time a limit, count or size, is hit.
+//
+// All integer arguments must be larger than zero. The size callback may be nil, in which case
+// all items will assumed to be of size 0.
+// To ignore size limits on batches, specify a sufficiently large value for batchSize.
+// The errors returned by this function are one of: ErrItemTooLarge, ErrInvalidArgument.
 func Make(itemCount int, batchLength int, batchSize int64, sizefn SizeFunc) ([][]int, error) {
 	if itemCount <= 0 || batchLength <= 0 || batchSize <= 0 {
-		return nil, errors.Join(ErrInvalidArgs, fmt.Errorf("invalid arguments: itemCount=%d, batchLength=%d, batchSize=%d", itemCount, batchLength, batchSize))
+		return nil, errors.Join(ErrInvalidArgument, fmt.Errorf("argument must be > 0, got: itemCount=%d, batchLength=%d, batchSize=%d", itemCount, batchLength, batchSize))
 	}
 
-	batches := [][]int{{}}
-	bi := 0
-	bs := int64(0)
+	var (
+		batches [][]int
+		bi      int
+		bs      int64
+	)
 
 	appendBatch := func() {
-		i := len(batches)
+		bi = len(batches)
 		batches = append(batches, []int{})
-		batches[i] = []int{}
 	}
 
+	appendBatch()
 	for i := 0; i < itemCount; i++ {
-		size := int64(0)
+		var size int64
 		if sizefn != nil {
 			size = sizefn(i)
 		}
 		if size > batchSize {
 			return nil, errors.Join(ErrItemTooLarge, fmt.Errorf("item at index %d has a size %d that exceeds the batchSize %d", i, size, batchSize))
 		}
+
 		if bs+size > batchSize {
+			appendBatch()
+		}
+
+		if len(batches[bi]) >= batchLength {
 			appendBatch()
 		}
 
 		batches[bi] = append(batches[bi], i)
 		bs += size
-
-		if len(batches[bi]) > batchLength {
-			appendBatch()
-		}
 	}
 	return batches, nil
 }
--- go/pkg/batcher/batcher_test.go
+++ go/pkg/batcher/batcher_test.go
@@ -3,56 +3,39 @@ package batcher
 import (
 	"errors"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBatcher(t *testing.T) {
-	_, err := Make(0, 0, 0, nil)
-	if err == nil || !errors.Is(err, ErrInvalidArgs) {
-		t.Errorf("want invalid args error, got %v", err)
+	tests := []struct {
+		name        string
+		count       int
+		length      int
+		size        int64
+		sizefn      SizeFunc
+		wantErr     error
+		wantBatches [][]int
+	}{
+		{"bad_arguments", 0, 0, 0, nil, ErrInvalidArgument, nil},
+		{"batch_of_one", 1, 1, 1, nil, nil, [][]int{{0}}},
+		{"one_batch_for_all", 5, 6, 1, nil, nil, [][]int{{0, 1, 2, 3, 4}}},
+		{"count_limit", 5, 1, 1, nil, nil, [][]int{{0}, {1}, {2}, {3}, {4}}},
+		{"odd_count_and_length", 11, 5, 1, nil, nil, [][]int{{0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}, {10}}},
+		{"size_limit", 5, 2, 1, func(i int) int64 { return int64(1) }, nil, [][]int{{0}, {1}, {2}, {3}, {4}}},
+		{"item_too_large", 5, 2, 2, func(i int) int64 { return int64(i) }, ErrItemTooLarge, nil},
 	}
 
-	batches, err := Make(1, 1, 1, nil)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if len(batches) < 1 {
-		t.Errorf("wrong batches: got %d, want %d", len(batches), 1)
-	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bs, err := Make(test.count, test.length, test.size, test.sizefn)
+			if !errors.Is(err, test.wantErr) {
+				t.Errorf("error mismatch: got %v, want %v", err, test.wantErr)
+			}
 
-	batches, err = Make(10, 1, 1, nil)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if len(batches) < 10 {
-		t.Errorf("wrong batches: got %d, want %d", len(batches), 1)
-	}
-
-	batches, err = Make(11, 5, 1, nil)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if len(batches) < 3 {
-		t.Errorf("wrong batches: got %d, want %d", len(batches), 3)
-	}
-
-	batches, err = Make(10, 5, 1, func(i int) int64 { return int64(1) })
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if len(batches) < 10 {
-		t.Errorf("wrong batches: got %d, want %d", len(batches), 10)
-	}
-
-	batches, err = Make(10, 5, 5, func(i int) int64 { return int64(2) })
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if len(batches) < 5 {
-		t.Errorf("wrong batches: got %d, want %d", len(batches), 5)
-	}
-
-	_, err = Make(10, 5, 2, func(i int) int64 { return int64(i) })
-	if err == nil || !errors.Is(err, ErrItemTooLarge) {
-		t.Errorf("want item too large error, got %v", err)
+			if diff := cmp.Diff(test.wantBatches, bs); diff != "" {
+				t.Errorf("stats mismatch, (-want +got): %s", diff)
+			}
+		})
 	}
 }
